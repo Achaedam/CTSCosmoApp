b8adef5b0bb7aa73946e588320db32c08a94f709
AnthonyUtt <anthonyutt@hotmail.com>
2017-04-24 20:01:05 -0400

    Almost finished! Version 0.9.1

---
+++ CosmetologyScheduling/CustCreate.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace CosmetologyScheduling
+{
+    partial class CustCreate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.custInfoBox = new System.Windows.Forms.GroupBox();
+            this.zipTextBox = new System.Windows.Forms.TextBox();
+            this.zipLabel = new System.Windows.Forms.Label();
+            this.stateTextBox = new System.Windows.Forms.TextBox();
+            this.stateLabel = new System.Windows.Forms.Label();
+            this.cityTextBox = new System.Windows.Forms.TextBox();
+            this.cityLabel = new System.Windows.Forms.Label();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.addressLabel = new System.Windows.Forms.Label();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.phoneTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.phoneLabel = new System.Windows.Forms.Label();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.custInfoBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // custInfoBox
+            // 
+            this.custInfoBox.Controls.Add(this.zipTextBox);
+            this.custInfoBox.Controls.Add(this.zipLabel);
+            this.custInfoBox.Controls.Add(this.stateTextBox);
+            this.custInfoBox.Controls.Add(this.stateLabel);
+            this.custInfoBox.Controls.Add(this.cityTextBox);
+            this.custInfoBox.Controls.Add(this.cityLabel);
+            this.custInfoBox.Controls.Add(this.addressTextBox);
+            this.custInfoBox.Controls.Add(this.addressLabel);
+            this.custInfoBox.Controls.Add(this.emailTextBox);
+            this.custInfoBox.Controls.Add(this.emailLabel);
+            this.custInfoBox.Controls.Add(this.phoneTextBox);
+            this.custInfoBox.Controls.Add(this.phoneLabel);
+            this.custInfoBox.Controls.Add(this.lastNameTextBox);
+            this.custInfoBox.Controls.Add(this.lastNameLabel);
+            this.custInfoBox.Controls.Add(this.firstNameTextBox);
+            this.custInfoBox.Controls.Add(this.firstNameLabel);
+            this.custInfoBox.Location = new System.Drawing.Point(12, 12);
+            this.custInfoBox.Name = "custInfoBox";
+            this.custInfoBox.Size = new System.Drawing.Size(381, 141);
+            this.custInfoBox.TabIndex = 0;
+            this.custInfoBox.TabStop = false;
+            this.custInfoBox.Text = "Enter customer information:";
+            // 
+            // zipTextBox
+            // 
+            this.zipTextBox.Location = new System.Drawing.Point(275, 107);
+            this.zipTextBox.Name = "zipTextBox";
+            this.zipTextBox.Size = new System.Drawing.Size(100, 20);
+            this.zipTextBox.TabIndex = 15;
+            // 
+            // zipLabel
+            // 
+            this.zipLabel.AutoSize = true;
+            this.zipLabel.Location = new System.Drawing.Point(244, 110);
+            this.zipLabel.Name = "zipLabel";
+            this.zipLabel.Size = new System.Drawing.Size(25, 13);
+            this.zipLabel.TabIndex = 14;
+            this.zipLabel.Text = "Zip:";
+            // 
+            // stateTextBox
+            // 
+            this.stateTextBox.Location = new System.Drawing.Point(197, 107);
+            this.stateTextBox.Name = "stateTextBox";
+            this.stateTextBox.Size = new System.Drawing.Size(33, 20);
+            this.stateTextBox.TabIndex = 13;
+            // 
+            // stateLabel
+            // 
+            this.stateLabel.AutoSize = true;
+            this.stateLabel.Location = new System.Drawing.Point(156, 110);
+            this.stateLabel.Name = "stateLabel";
+            this.stateLabel.Size = new System.Drawing.Size(35, 13);
+            this.stateLabel.TabIndex = 12;
+            this.stateLabel.Text = "State:";
+            // 
+            // cityTextBox
+            // 
+            this.cityTextBox.Location = new System.Drawing.Point(39, 107);
+            this.cityTextBox.Name = "cityTextBox";
+            this.cityTextBox.Size = new System.Drawing.Size(100, 20);
+            this.cityTextBox.TabIndex = 11;
+            // 
+            // cityLabel
+            // 
+            this.cityLabel.AutoSize = true;
+            this.cityLabel.Location = new System.Drawing.Point(6, 110);
+            this.cityLabel.Name = "cityLabel";
+            this.cityLabel.Size = new System.Drawing.Size(27, 13);
+            this.cityLabel.TabIndex = 10;
+            this.cityLabel.Text = "City:";
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(60, 79);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(315, 20);
+            this.addressTextBox.TabIndex = 9;
+            // 
+            // addressLabel
+            // 
+            this.addressLabel.AutoSize = true;
+            this.addressLabel.Location = new System.Drawing.Point(6, 82);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(48, 13);
+            this.addressLabel.TabIndex = 8;
+            this.addressLabel.Text = "Address:";
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(249, 51);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(126, 20);
+            this.emailTextBox.TabIndex = 7;
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Location = new System.Drawing.Point(208, 54);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(35, 13);
+            this.emailLabel.TabIndex = 6;
+            this.emailLabel.Text = "Email:";
+            // 
+            // phoneTextBox
+            // 
+            this.phoneTextBox.Location = new System.Drawing.Point(95, 51);
+            this.phoneTextBox.Mask = "(000)000-0000";
+            this.phoneTextBox.Name = "phoneTextBox";
+            this.phoneTextBox.Size = new System.Drawing.Size(77, 20);
+            this.phoneTextBox.TabIndex = 5;
+            // 
+            // phoneLabel
+            // 
+            this.phoneLabel.AutoSize = true;
+            this.phoneLabel.Location = new System.Drawing.Point(6, 54);
+            this.phoneLabel.Name = "phoneLabel";
+            this.phoneLabel.Size = new System.Drawing.Size(81, 13);
+            this.phoneLabel.TabIndex = 4;
+            this.phoneLabel.Text = "Phone Number:";
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(275, 22);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.lastNameTextBox.TabIndex = 3;
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(208, 25);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(61, 13);
+            this.lastNameLabel.TabIndex = 2;
+            this.lastNameLabel.Text = "Last Name:";
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(72, 22);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.firstNameTextBox.TabIndex = 1;
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Location = new System.Drawing.Point(6, 25);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(60, 13);
+            this.firstNameLabel.TabIndex = 0;
+            this.firstNameLabel.Text = "First Name:";
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(318, 168);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "&Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(237, 168);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(75, 23);
+            this.submitButton.TabIndex = 2;
+            this.submitButton.Text = "&Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // CustCreate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 203);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.custInfoBox);
+            this.Name = "CustCreate";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Create Customer";
+            this.custInfoBox.ResumeLayout(false);
+            this.custInfoBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox custInfoBox;
+        private System.Windows.Forms.MaskedTextBox phoneTextBox;
+        private System.Windows.Forms.Label phoneLabel;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.TextBox zipTextBox;
+        private System.Windows.Forms.Label zipLabel;
+        private System.Windows.Forms.TextBox stateTextBox;
+        private System.Windows.Forms.Label stateLabel;
+        private System.Windows.Forms.TextBox cityTextBox;
+        private System.Windows.Forms.Label cityLabel;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.Label addressLabel;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button submitButton;
+    }
+}
---
+++ CosmetologyScheduling/CustCreate.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SCCCosmetology;
+using System.Text.RegularExpressions;
+
+namespace CosmetologyScheduling
+{
+    public partial class CustCreate : Form
+    {
+        Customer newCust;
+        public CustCreate()
+        {
+            InitializeComponent();
+        }
+
+        private string FormatPhone(string phone)
+        {
+            //Creates a Regular Expression to remove symbols from phone number
+            Regex rgx = new Regex("[() -]");
+            string phoneFixed = rgx.Replace(phone, "");
+
+            //Returns the fixed phone number
+            return phoneFixed;
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            string phone = FormatPhone(phoneTextBox.Text.ToString());
+
+            if (!string.IsNullOrEmpty(firstNameTextBox.Text.ToString()))
+            {
+                if (!string.IsNullOrEmpty(lastNameTextBox.Text.ToString()))
+                {
+                    if (!string.IsNullOrEmpty(phone))
+                    {
+                        newCust = new Customer();
+
+                        newCust.FirstName = firstNameTextBox.Text.ToString();
+                        newCust.LastName = lastNameTextBox.Text.ToString();
+                        newCust.Phone = phone;
+                        newCust.Email = emailTextBox.Text.ToString();
+                        newCust.Address = addressTextBox.Text.ToString();
+                        newCust.City = cityTextBox.Text.ToString();
+                        newCust.State = stateTextBox.Text.ToString();
+                        newCust.Zip = zipTextBox.Text.ToString();
+
+                        if (newCust.SendToDB())
+                        {
+                            this.Close();
+                        }
+                        else
+                        {
+                            MessageBox.Show("There was an error in communicating with the database. Please try again later.");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Phone Number field is required.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Last Name field is required.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("First Name field is required.");
+            }
+        }
+
+        public Customer NewCust
+        {
+            get { return newCust; }
+        }
+    }
+}
